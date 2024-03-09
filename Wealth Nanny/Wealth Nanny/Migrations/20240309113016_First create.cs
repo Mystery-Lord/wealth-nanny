bd7d55df8bd602bb9a65b41130786f1311af9226
lebanap <abelpan2022@gmail.com>
2024-03-09 21:22:21 +0800

    1.Configure the initial local databse. 2. Initial project testing

---
+++ Wealth Nanny/Wealth Nanny/Migrations/20240309113016_First create.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Wealth_Nanny.Migrations
+{
+    public partial class Firstcreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
